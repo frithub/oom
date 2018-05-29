995520d5f7e6bf53527bfdeb302ea2087b9edb99
F <fritz.schuelein@gmx.de>
2018-05-29 19:31:37 +0200

    add task4

---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.IO;
+
+
+
+namespace Task4
+{
+    public interface Icomponent
+    {
+        void showTurnedOn(bool powerOn); //arguments?
+        void showDevice();
+    }
+    public class Switch : Icomponent
+    {
+        private int power;
+        public bool turnedOn;
+        private string name;
+        private ushort interfaces;
+        private UInt32 bandwithMBit;
+
+        public Switch(ushort theInterfaces, UInt32 theBandwithMBit, int watt, bool on, string switchName)
+        {
+
+            if (theInterfaces == 0)
+                throw new Exception("Keine Interfaces.");
+            if (theBandwithMBit == 0)
+                throw new Exception("Ungültige Bandbreite.");
+            interfaces = theInterfaces;
+            bandwithMBit = theBandwithMBit;
+            power = watt;
+            turnedOn = on;
+            name = switchName;
+        }
+
+
+        public void showDevice()
+        {
+            Console.WriteLine("this is the switch ... " + name); // name?
+        }
+        public void showTurnedOn(bool powerOn)
+        {
+            if (powerOn == true)
+                Console.WriteLine("Switched on.");
+            else
+                Console.WriteLine("Switched off.");
+        }
+        public ushort Interfaces
+        {
+            get { return interfaces; }
+        }
+        public UInt32 BandwithMBit
+        {
+            get { return bandwithMBit; }
+        }
+    }
+
+
+
+    public class Router : Icomponent
+    {
+        private int power;
+        private bool turnedOn;
+        private string name;
+
+        public void showDevice()
+        {
+            Console.WriteLine("this is the router ...  "); // name?
+        }
+        public void showTurnedOn(bool powerOn)
+        {
+            if (powerOn == true)
+                Console.WriteLine("Switched on.");
+            else
+                Console.WriteLine("Switched off.");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            Icomponent[] devices = new Icomponent[3];
+            devices[0] = new Switch(24, 1000, 110, false, "Catalyst2960-L");
+            devices[1] = new Router();
+            devices[2] = new Switch(24, 1000, 110, false, "Catalyst2960-X");
+
+            // Seralize
+            for (int i = 0; i < devices.Length; i++)
+            {
+                string json = JsonConvert.SerializeObject(devices[i], Formatting.Indented);
+
+                File.WriteAllText(path + "object" + i + ".json", json);
+
+                
+            }
+
+            for (int i = 0; i < devices.Length; i++)
+            {
+                string json = File.ReadAllText(path + "object" + i + ".json");
+                Console.WriteLine(json);
+            }
+
+
+                Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tasks/Task4/Task4/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task4
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void SimpleTest2()
+        {
+            Assert.IsTrue(1 == 0);
+        }
+    }
+}
